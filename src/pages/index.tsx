69c88eae8e14e7473ede58282ca3cc1bf64082f4
Joao Dias <joaoeduardodias123@gmail.com>
2021-12-10 15:20:10 -0300

    feat: styles pag home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { GetStaticProps } from 'next';
-
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Header from '../components/Header';
 import { getPrismicClient } from '../services/prismic';
-
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -24,9 +25,54 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Header />
+      <main className={`${commonStyles.container} ${styles.postContainer}`}>
+        <Link href="/">
+          <a className={styles.post}>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <ul>
+              <li>
+                <time>
+                  <FiCalendar />
+                  15 Mar 2021
+                </time>
+              </li>
+              <li>
+                <FiUser />
+                João Dias
+              </li>
+            </ul>
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.post}>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <ul>
+              <li>
+                <time>
+                  <FiCalendar />
+                  15 Mar 2021
+                </time>
+              </li>
+              <li>
+                <FiUser />
+                João Dias
+              </li>
+            </ul>
+          </a>
+        </Link>
+        <button type="button" className={styles.loadPosts}>
+          Carregar mais posts
+        </button>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
